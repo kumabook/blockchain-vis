22aa4cdfc78bcdb07f976747d8041dfafe776e9b
Hiroki Kumamoto <kumabook@live.jp>
2018-03-07 17:09:04 +0900

    Implement basic http api: `POST /transaction/new`, `GET /mine`, `GET /chain`

--- src/Blockchain.js
+++ src/Blockchain.js
@@ -1,6 +1,9 @@
 // @flow
 
 import crypto from 'crypto';
+import uuidv4 from 'uuid/v4';
+import express from 'express';
+import bodyParser from 'body-parser';
 
 type Transaction = {
   sender:    string,
@@ -83,3 +86,37 @@ export class Blockchain {
     return this.chain[this.chain.length - 1];
   }
 }
+
+const app = express();
+const blockchain = new Blockchain()
+const nodeIdentifier = uuidv4().replace('-', '');
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.get('/mine', (req, res) => {
+  const { lastBlock } = blockchain;
+  const { proof } = lastBlock;
+  blockchain.newTransaction('0', nodeIdentifier, 1);
+  const block = blockchain.newBlock(proofOfWork(proof), hashOfBlock(lastBlock))
+  res.json({
+    message:      'New Block Forged',
+    index:        block.index,
+    transactions: block.transactions,
+    proof:        block.proof,
+    previousHash: block.previousHash,
+  });
+});
+
+app.post('/transactions/new', (req, res) => {
+  const { sender, recipient, amount } = req.body;
+  const index = blockchain.newTransaction(sender, recipient,  amount);
+  res.status(201)
+    .json({ message: `Transaction will be added to Block ${index}` });
+});
+
+app.get('/chain', (req, res) => res.json({
+  chain:  blockchain.chain,
+  length: blockchain.chain.length,
+}));
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
